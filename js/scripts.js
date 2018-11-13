3913b9fe130abcbb9831752df9da05f69c21dbe0
Stewart ilondanga <stilondanga@gmail.com>
2018-11-13 16:57:17 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,29 +33,29 @@ jQuery(document).ready(function($) {
       $('#slider ul li:last-child').prependTo('#slider ul');
 
       function moveLeft() {
-        /*                    $('#slider ul').animate({
-                                left: + slideWidth
-                            }, 200, function () {
-                                $('#slider ul li:last-child').prependTo('#slider ul');
-                                $('#slider ul').css('left', '');
+        $('#slider ul').animate({
+              left: +slideWidth
+              /*                      }, 200, function () {
+                                        $('#slider ul li:last-child').prependTo('#slider ul');
+                                        $('#slider ul').css('left', '');
+                                    });
+                                };
+
+                                function moveRight() {
+                                    $('#slider ul').animate({
+                                        left: - slideWidth
+                                    }, 200, function () {
+                                        $('#slider ul li:first-child').appendTo('#slider ul');
+                                        $('#slider ul').css('left', '');
+                                    });
+                                };
+
+                                $('a.control_prev').click(function () {
+                                    moveLeft();
+                                });
+
+                                $('a.control_next').click(function () {
+                                    moveRight();
+                                });
+
                             });
-                        };
-
-                        function moveRight() {
-                            $('#slider ul').animate({
-                                left: - slideWidth
-                            }, 200, function () {
-                                $('#slider ul li:first-child').appendTo('#slider ul');
-                                $('#slider ul').css('left', '');
-                            });
-                        };
-
-                        $('a.control_prev').click(function () {
-                            moveLeft();
-                        });
-
-                        $('a.control_next').click(function () {
-                            moveRight();
-                        });
-
-                    });
