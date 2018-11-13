ed4e86557751d55fb43918586dbef76618abdaf1
Stewart ilondanga <stilondanga@gmail.com>
2018-11-13 17:02:02 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -34,28 +34,28 @@ jQuery(document).ready(function($) {
 
       function moveLeft() {
         $('#slider ul').animate({
-              left: +slideWidth
-              /*                      }, 200, function () {
-                                        $('#slider ul li:last-child').prependTo('#slider ul');
-                                        $('#slider ul').css('left', '');
-                                    });
-                                };
+          left: +slideWidth
+        }, 200, function() {
+          $('#slider ul li:last-child').prependTo('#slider ul');
+          $('#slider ul').css('left', '');
+        });
+      };
 
-                                function moveRight() {
-                                    $('#slider ul').animate({
-                                        left: - slideWidth
-                                    }, 200, function () {
-                                        $('#slider ul li:first-child').appendTo('#slider ul');
-                                        $('#slider ul').css('left', '');
-                                    });
-                                };
+      /*                        function moveRight() {
+                                  $('#slider ul').animate({
+                                      left: - slideWidth
+                                  }, 200, function () {
+                                      $('#slider ul li:first-child').appendTo('#slider ul');
+                                      $('#slider ul').css('left', '');
+                                  });
+                              };
 
-                                $('a.control_prev').click(function () {
-                                    moveLeft();
-                                });
+                              $('a.control_prev').click(function () {
+                                  moveLeft();
+                              });
 
-                                $('a.control_next').click(function () {
-                                    moveRight();
-                                });
+                              $('a.control_next').click(function () {
+                                  moveRight();
+                              });
 
-                            });
+                          });
